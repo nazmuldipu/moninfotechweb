4f7f25b6a5746806e33d25f3a313d545519b9170
Nazmul Alam <nazmuldipu@gmail.com>
2018-06-19 13:10:54 +0600

    career added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { CareerComponent } from './career/career.component';
 
+// routes
+export const ROUTES: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'career',
+    component: CareerComponent
+  }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule.forRoot()
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, CareerComponent],
+  imports: [BrowserModule, NgbModule.forRoot(), RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
